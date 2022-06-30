22bf822c8d30aef672bfdf43c9a64d69b102161a
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-30 16:39:29 +0200

    generatore num seriale per i dvd

--- DVD.cs
+++ DVD.cs
@@ -10,8 +10,24 @@ namespace csharp_biblioteca
     {
         public DVD(string title) : base(title)
         {
+            base.id = this.GenerateCode();
         }
 
         public int Durata { get; set; }
+
+        private string GenerateCode()
+        {
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var Charsarr = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < Charsarr.Length; i++)
+            {
+                Charsarr[i] = characters[random.Next(characters.Length)];
+            }
+
+            var resultString = new String(Charsarr);
+            return resultString;
+        }
     }
 }
