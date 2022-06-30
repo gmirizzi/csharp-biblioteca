61de9ee6bdb3df90960619e2da473f1f41a4bde3
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-30 14:20:00 +0200

    create le classi documento, autore e le sottoclassi dvd e libro

---
+++ Documento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Documento
+    {
+        protected string id;
+        protected string title;
+        protected int year;
+        protected string settore;
+        protected string stato;
+        protected int scaffale;
+        protected Autore author;
+    }
+}
